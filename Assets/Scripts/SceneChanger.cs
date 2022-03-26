17adb1e212083749b303ed0b178b52129f6bdd32
CianOToole <cianotoole4@gmail.com>
2022-03-26 00:50:47 +0000

    scene changing

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,10 +6,28 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
 
-
+    public static int loadInt = 1;
     public void playGame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(loadInt);
     }
-    
+
+    public void setSceneOne()
+    {
+        loadInt = 1;
+        SceneManager.LoadScene(0);
+    }
+
+    public void setSceneTwo()
+    {
+        loadInt = 2;
+        SceneManager.LoadScene(5);
+    }
+
+    public void setSceneThree()
+    {
+        loadInt = 4;
+        SceneManager.LoadScene(3);
+    }
+
 }
